3c572fd34c52a5cdf93e9c2d00512d6661cb90f8
basma <bsalim@constructor.university>
2026-01-12 22:45:16 +0100

    commit to new repo deploy3

--- bcend-py/server.py
+++ bcend-py/server.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 import joblib
-import os
+import 
 
 app = Flask(__name__)
 
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Path to your model folder and file
 MODEL_PATH = os.path.join(BASE_DIR, "model", "spam_model.joblib")
-model = joblib.load("/model/spam_model.joblib")
+model = joblib.load(MODEL_PATH)
 vectorizer = joblib.load("/model/vectorizer.joblib")
 
 @app.route("/analyze", methods=["POST"])
